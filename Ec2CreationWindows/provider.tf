52e8d7036bf249faa8053482bca3bbe476d2c39a
Divya narayanagari <djspy.java@gmail.com>
2022-01-19 08:48:06 +0530

    Delete Ec2CreationWindows directory

--- Ec2CreationWindows/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  access_key = "${var.aws_access_key}" # Use the access key from downloaded csv file.
-  secret_key = "${var.aws_secret_key}" # Use the secret key from downloaded csv file.
-  region     = "${var.aws_region}"
-}
